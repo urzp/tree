4b6c86fff2402391cdb639823d40a0762ae2087e
urzp <urzp002@mail.ru>
2016-06-28 17:33:32 +0300

    2 step create recursion

--- tree.rb
+++ tree.rb
@@ -3,10 +3,10 @@ class Node
   attr :parent
   attr :childs
 
-  def initialize(value)
+  def initialize(value, parent = nil, left_child = nil, right_child = nil)
     @value = value
-    @parent = nil
-    @childs = {left:nil, right:nil}
+    @parent = parent
+    @childs = {left:left_child, right:right_child}
   end
 
   def get_parent
@@ -32,17 +32,29 @@ class Node
   def set_right_child=(value)
     @childs[:right] = value
   end
-
-
 end
 
 def build_tree(array)
+  puts array.size
+  
+  if array.size == 1
+    name_node = array[1]
+    left_arr = array[0]
+    right_arr = nil
+    $tree << Node.new(name_node)
+  end
+
+  if array.size > 1
+      name_node = array[array.size/2]
+      left_arr = build_tree(array.slice(0,array.size/2 - 1))
+      right_arr = build_tree(array.slice(array.size/2 + 1, array.size))
+      $tree << Node.new(name_node)
+  end
 
 end
 
 #------
-
-test = Node.new(8)
-test.set_parent=10
-test.set_right_child= 5
-puts test.get_right_child
+$tree = []
+build_tree([1,2,3,4,6,7,8,9,10])
+print $tree
+puts
