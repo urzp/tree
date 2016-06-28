1c019eeaacbe18a9fe36578fca593cdc63e33f4b
urzp <urzp002@mail.ru>
2016-06-28 19:12:35 +0300

    2 level create a tree

--- tree.rb
+++ tree.rb
@@ -34,27 +34,47 @@ class Node
   end
 end
 
-def build_tree(array)
-  puts array.size
+def build_tree(array, parent = nil, derection = nil)
+  print "#{array} size=#{array.size}  parent=#{parent} dir=#{derection}"
+  puts
   
-  if array.size == 1
+  if array.size == 2
+    if derection == :left or derection == nil
+      name_node = array[1]
+      left_child = array[0]
+      right_child = nil
+	else
+	  name_node = array[0]
+      left_child = nil
+      right_child = array[1]	  
+	end
+    $tree << Node.new(name_node, parent, left_child, right_child)
+  end
+  
+  if array.size == 3
     name_node = array[1]
-    left_arr = array[0]
-    right_arr = nil
-    $tree << Node.new(name_node)
+    left_child = array[0]
+    right_child = array[2]
+    $tree << Node.new(name_node, parent, left_child, right_child)
   end
 
-  if array.size > 1
-      name_node = array[array.size/2]
-      left_arr = build_tree(array.slice(0,array.size/2 - 1))
-      right_arr = build_tree(array.slice(array.size/2 + 1, array.size))
-      $tree << Node.new(name_node)
-  end
+  if array.size > 3
+    name_node = array[array.size/2]
+	left = array.slice(0,array.size/2 - 1)
+	right = array.slice(array.size/2 + 1, array.size)
+	left_child = left[left.size/2]
+	right_child = right[right.size/2]
+	$tree << Node.new(name_node, parent, left_child, right_child)
+
+    build_tree(array.slice(0,array.size/2), name_node, :left)  
+    build_tree(array.slice(array.size/2 + 1, array.size), name_node, :right)
+  end    
+  
 
 end
 
 #------
 $tree = []
-build_tree([1,2,3,4,6,7,8,9,10])
+build_tree([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
 print $tree
 puts
