c6359814f3e2b783e0dce84af2d00e2a6c642b07
urzp <urzp002@mail.ru>
2016-06-28 23:46:19 +0300

    addand test breadth_first_search

--- tree.rb
+++ tree.rb
@@ -83,12 +83,23 @@ end
 
 def breadth_first_search(tree, value)
   visited = []
-private
-  def found?(value, node)
-    node if value == node.value
-	node if node.childs[:left] == value
-  end  
+  $tree.each do |node|
+    unless visited.any?{|i| i == node or i == node.get_left_child or i = node.get_right_child }
+      return found?(value, node) if found?(value, node)
+	  visited << node
+	end  
+  end
+  return nil
+  
 end
+
+def found?(value, node)
+    return node if value == node.value
+	return node.childs[:left] if node.childs[:left] != nil and node.childs[:left].value == value
+	return node.childs[:right] if node.childs[:right] != nil and node.childs[:right].value == value
+	return false
+end  
+
 
 
 
@@ -103,3 +114,5 @@ $tree.each do |i|
   print "parent=#{i.parent} \t val=#{i.value}  \t chld   left=#{i.childs[:left].value unless i.childs[:left] == nil}    \t  right=#{i.childs[:right].value unless i.childs[:right] == nil}  "
   puts
 end
+puts
+puts breadth_first_search($tree, 15).value
